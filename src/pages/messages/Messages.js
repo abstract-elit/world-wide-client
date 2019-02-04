63c93462d089808ccfc03c86e213bd354ce60c1d
lupuselit <lupuselit@outlook.com>
2019-02-04 11:19:19 -0200

    fix

--- src/pages/messages/Messages.js
+++ src/pages/messages/Messages.js
@@ -311,12 +311,12 @@ class Messages extends Component {
       'Nov',
       'Dec'
     ];
-    var year = a.getFullYear();
+    // var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
     var min = a.getMinutes();
-    var sec = a.getSeconds();
+    // var sec = a.getSeconds();
 
     // var time =
     //   date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
